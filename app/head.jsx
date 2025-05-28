ab4bfb4a71256ecb82e46edbe7ad48b1b8e70e2f
Diego Artacho <diegoartacho97@gmail.com>
2025-05-28 17:40:34 +0100

    .

---
+++ app/head.jsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Diego Artacho</title>
+      <meta name="description" content="PhD student in Mathematics at Imperial College London" />
+      <meta name="google-site-verification" content="LTn_Nq7NGtPnfx3Qf6FG8ySLmsaGVtA6CsvyLIWKVQA" />
+    </>
+  );
+}
